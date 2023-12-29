302524cf1d200612ff5b4c611d814d499b8ca863
Andromeda-h <gian.chiariello@gmail.com>
2023-12-29 20:01:09 -0300

    V1.1.2

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -17,7 +17,7 @@ const Start = () => {
 
     return (
         <div className="w-full h-full bg-white dark:bg-zinc-900 dark:text-white">
-            <p>actualizado</p>
+            <p>actualizado v2</p>
             {theme} <br />
             <button onClick={() => changeTheme()}>
                 {t('test')}
