a50a7d631800a70c39dbfe936bae90b60dfc923f
Andromeda-h <gian.chiariello@gmail.com>
2023-12-30 21:37:04 -0300

    v1.1.12

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,22 +1,29 @@
 import { interfaceStore } from "../state/stores/interface.store"
 import { useNavigate } from 'react-router-dom'
 import { getVersion } from '@tauri-apps/api/app';
+import { useState, useEffect } from "react";
 
 //tests
 
 const Start = () => {
     const navigate = useNavigate();
     const { lang } = interfaceStore();
-
+    const [version, setVersion] = useState("")
 
     const getVersionApp = async () => {
-        return await getVersion();
+        const version = await getVersion();
+        setVersion(version)
     }
+
+    useEffect(() => {
+        getVersionApp()
+    }, [])
+
 
 
     return (
         <div className="w-full h-full bg-white dark:bg-zinc-900 dark:text-white">
-            <p>pre actualizado {getVersionApp() as any}</p>
+            <p>pre actualizado {version}</p>
             <br /><br />
             <button onClick={() => navigate('/dashboard')}>
                 To dashboard {lang}
